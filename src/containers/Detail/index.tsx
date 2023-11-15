2576f1f73b5b0d1e2550ebd5c2c5afb8898c2b1f
1134701187@qq.com <1134701187@qq.com>
2023-11-15 16:13:07 +0800

    整理代码

--- src/containers/Detail/index.tsx
+++ src/containers/Detail/index.tsx
@@ -6,24 +6,19 @@ import useRequest from "@/hooks/useRequest";
 
 const requestData = {
   url: "https://www.fastmock.site/mock/f307fca25de6a901228480d6513e9950/api/detail",
-    method: "GET",
-    params: {
-      id: ""
-    },
-    manual: true,
-}
+  method: "GET",
+  params: {
+    id: "",
+  },
+  manual: true,
+};
 const Detail = () => {
   const navigate = useNavigate();
-  const params = useParams<{id: string}>()
-  requestData.params.id = params?.id
-  // const [requestData, setRequestData] = useState({
-    
-  // })
-  const {data} = useRequest<ResponseType>(requestData)
-  const result = data?.data
+  const params = useParams<{ id: string }>();
+  requestData.params.id = params?.id;
+  const { data } = useRequest<ResponseType>(requestData);
+  const result = data?.data;
   console.log(data);
-  
-
 
   return (
     <div className="page detail-page">
@@ -38,14 +33,11 @@ const Detail = () => {
         </div>
         商品详情
       </div>
-      <img
-        className="image"
-        src={result?.imgUrl}
-        alt=""
-      />
+      <img className="image" src={result?.imgUrl} alt="" />
       <div className="main">
         <div className="main-price">
-          <span className="main-price-yen">&yen;</span>{result?.price}
+          <span className="main-price-yen">&yen;</span>
+          {result?.price}
         </div>
         <div className="main-sales">已售{result?.sales}</div>
         <div className="main-content">
@@ -68,10 +60,7 @@ const Detail = () => {
       </div>
       <div className="detail">
         <div className="detail-title">商品详情</div>
-        <div className="detail-content">
-          {result?.detail}
-          {/* 就得啦荆防颗粒电视剧俯卧内容按流程VN拉三等奖佛为UR缇欧恶如对方可理解阿斯利康地方近段时间法律可适当 */}
-        </div>
+        <div className="detail-content">{result?.detail}</div>
       </div>
       <div className="docker">
         <div className="cart-icon">
